33a22e2dc66696aed747906fb313cd36af319669
tomas unzue <tomas.unzue03@gmail.com>
2023-06-06 23:59:58 -0300

    correccion-proyectofinal

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,18 @@ import Cart from './components/Cart/Cart';
 import {CartProvider} from './context/CartContext'
 
 
-
-
 function App (){
     return(
       <div className= "App">
         <BrowserRouter>
           <NavBar/>
-          <Routes>
-            <Route path='/' element={<ItemListContainer/>}/>
-            <Route path='/category/categoryId' element={<ItemListContainer/>}/>
-            <Route path='item/:itemId' element={<ItemDetailContainer/>}/>
-            <Route path='/cart' element={<Cart/>}/>
-            <Route path='*' element={ItemListContainer}/>
-          </Routes>
+            <Routes>
+              <Route path='/' element={<ItemListContainer/>}/>
+              <Route path='/category/categoryId' element={<ItemListContainer/>}/>
+              <Route path='item/:itemId' element={<ItemDetailContainer/>}/>
+              <Route path='/cart' element={<Cart/>}/>
+              <Route path='*' element={ItemListContainer}/>
+            </Routes>
           <CartProvider/>
         </BrowserRouter>
       </div>
